eeb7b06622b17409242eaa3e22126c157d44ae85
Raveen K Sharma <raveen08@gmail.com>
2020-05-09 17:23:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,3 +10,11 @@ def reverse_each_word(words)
   reverse_sentence.join(" ")
 end
 
+def collect_each_word(words)
+  sentence = words.split(" ")
+  reverse_sentence = []
+  sentence.each do |word|
+    reverse_sentence << word.reverse
+  end
+  reverse_sentence.join(" ")
+end
